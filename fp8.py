28d81b0a9e1c5bf6251116f10fbede6c00ed59f8
Matthew Hodges <52683582+matthewhodges56@users.noreply.github.com>
2024-10-31 14:44:46 -0500

    made good progress on gui

--- fp8.py
+++ fp8.py
@@ -4,12 +4,25 @@ root = Tk()
 root.geometry("500x350")
 root.title("Customer Feedback")
 
-titleLabel = StringVar()
-titleLabel.set("Please provide feedback on your experience.")
+titleLabel = ttk.Label(root, text='Please provide feedback on your experience.', font=("Arial", 14, "bold"))
+titleLabel.pack(pady=30)
 
-label = ttk.Label(root, textvariable=titleLabel, font=("Arial", 14, "bold"),)
-label.pack(pady=35)
+nameLabel = ttk.Label(root, text="Name:", font=("Arial", 10, "normal"))
+nameLabel.place(relx=0.35, rely=0.3, anchor='center')
 
+nameEntry = ttk.Entry(root)
+nameEntry.place(relx=0.53, rely=0.3, anchor='center')
 
+emailLabel = ttk.Label(root, text="Email:", font=("Arial", 10, "normal"))
+emailLabel.place(relx=0.35, rely=0.4, anchor='center')
+
+emailEntry = ttk.Entry(root)
+emailEntry.place(relx=0.53, rely=0.4, anchor='center')
+
+feedbackLabel = ttk.Label(root, text="Feedback:", font=("Arial", 10, "normal"))
+feedbackLabel.place(relx=0.325, rely=0.5, anchor='center')
+
+feedbackEntry = ttk.Entry(root, width=40)
+feedbackEntry.place(relx=0.6, rely=0.5, anchor='center')
 
 root.mainloop()
